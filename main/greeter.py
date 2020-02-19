c04a8a1d67e7e5e9c0df37421ff4d3ad6c8eddcb
Sorathesky117 <cesar.medrano@cardinalhealth.com>
2020-02-19 14:06:47 -0500

    fleshing out the greeter file from the info in the tutorial, going to work on another project, pausing for now

--- main/greeter.py
+++ main/greeter.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 # Greeter is a terminal application that greets old friends warmly,
 #   and remembers new friends.
 
@@ -5,3 +7,15 @@
 print("\t**********************************************")
 print("\t***  Greeter - Hello old and new friends!  ***")
 print("\t**********************************************")
+
+# Lets print a bunch of info in short intervals
+names = ['Aaron', 'Brenda', 'Cyrene', 'David', 'eric']
+
+# Prints each name 5 times
+for name in names:
+    # Pause for one second between cycles and then add two lines to the CLI
+    sleep(1)
+    print("\n\n")
+
+    for x in range(0, 5):
+        print(name.title())
