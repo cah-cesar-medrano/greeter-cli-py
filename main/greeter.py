8b128d90f073265d32dce472aba1df2b6a4e592c
Sorathesky117 <cesar.medrano@cardinalhealth.com>
2020-02-24 10:24:21 -0500

    added a function for a choice menu, and switch style if statement outcomes

--- main/greeter.py
+++ main/greeter.py
@@ -4,6 +4,7 @@ import os
 # Greeter is a terminal application that greets old friends warmly,
 #   and remembers new friends.
 
+### FUNCTIONS ###
 
 def display_title_bar():
     # Clears cli between cycles and displays title bar
@@ -12,6 +13,33 @@ def display_title_bar():
     print("\t**********************************************")
     print("\t***  Greeter - Hello old and new friends!  ***")
     print("\t**********************************************")
+
+
+def get_user_choice():
+    # let users know what they can do.
+    print("\n[1] See a list of friends.")
+    print("[2] Tell me about someone new.")
+    print("[q] Quit.\n")
+
+    return input("What would you like to do?")
+### PROGRAM ### 
+
+# Set up a loop where users can choose what they'd like to do.
+choice = ''
+display_title_bar()
+while choice != 'q':
+
+    # Response to user choice
+    display_title_bar()
+    if choice == '1':
+        print("\nHere are the people I know.\n")
+    elif choice == '2':
+        print("\nI can't wait to meet this person!\n")
+    elif choice == 'q':
+        print("\nThanks for playing. Bye.")
+    else:
+        print("\nI didn't understand that choice.\n")
+
 
 # Lets print a bunch of info in short intervals
 names = ['Aaron', 'Brenda', 'Cyrene', 'David', 'eric']
