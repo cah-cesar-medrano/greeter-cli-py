3ff24664dc2e94c234137c8b971b6eec3adc301d
Sorathesky117 <cesar.medrano@cardinalhealth.com>
2020-02-24 10:12:20 -0500

    removed duplicate code for title and clear and added a function to make it reusable as it may be needed on other screens

--- main/greeter.py
+++ main/greeter.py
@@ -4,27 +4,24 @@ import os
 # Greeter is a terminal application that greets old friends warmly,
 #   and remembers new friends.
 
-# Display a title bar.
-print("\t**********************************************")
-print("\t***  Greeter - Hello old and new friends!  ***")
-print("\t**********************************************")
+
+def display_title_bar():
+    # Clears cli between cycles and displays title bar
+    os.system('cls')
+    # Display a title bar.
+    print("\t**********************************************")
+    print("\t***  Greeter - Hello old and new friends!  ***")
+    print("\t**********************************************")
 
 # Lets print a bunch of info in short intervals
 names = ['Aaron', 'Brenda', 'Cyrene', 'David', 'eric']
 
 # Prints each name 5 times
 for name in names:
-    # Pause for one second between cycles and then add two lines to the CLI
-    os.system('cls')
+    display_title_bar()
 
-    # Display a title bar.
-    print("\t**********************************************")
-    print("\t***  Greeter - Hello old and new friends!  ***")
-    print("\t**********************************************")
 
-    sleep(1)
     print("\n\n")
-
     for x in range(0, 5):
         print(name.title())
     
