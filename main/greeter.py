6ea68a3ff8a756e45875f93fe4174d5630c5f547
Sorathesky117 <cesar.medrano@cardinalhealth.com>
2020-02-24 10:06:39 -0500

    added title to loop and cleared screen between cycles

--- main/greeter.py
+++ main/greeter.py
@@ -1,4 +1,5 @@
 from time import sleep
+import os 
 
 # Greeter is a terminal application that greets old friends warmly,
 #   and remembers new friends.
@@ -14,8 +15,18 @@ names = ['Aaron', 'Brenda', 'Cyrene', 'David', 'eric']
 # Prints each name 5 times
 for name in names:
     # Pause for one second between cycles and then add two lines to the CLI
+    os.system('cls')
+
+    # Display a title bar.
+    print("\t**********************************************")
+    print("\t***  Greeter - Hello old and new friends!  ***")
+    print("\t**********************************************")
+
     sleep(1)
     print("\n\n")
 
     for x in range(0, 5):
         print(name.title())
+    
+    # Pause for 1 second between cycles
+    sleep(1)
