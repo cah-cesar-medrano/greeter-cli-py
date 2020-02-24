4872bf44a76c1f0fcf9691a5e67c6c508ab4ab4e
Sorathesky117 <cesar.medrano@cardinalhealth.com>
2020-02-24 10:40:39 -0500

    Added empty list, added printing of names, and adding of names

--- main/greeter.py
+++ main/greeter.py
@@ -1,10 +1,11 @@
-from time import sleep
-import os 
+# from time import sleep
+import os
 
 # Greeter is a terminal application that greets old friends warmly,
 #   and remembers new friends.
 
-### FUNCTIONS ###
+# FUNCTIONS ###
+
 
 def display_title_bar():
     # Clears cli between cycles and displays title bar
@@ -22,36 +23,29 @@ def get_user_choice():
     print("[q] Quit.\n")
 
     return input("What would you like to do?")
-### PROGRAM ### 
+# PROGRAM ###
+
 
 # Set up a loop where users can choose what they'd like to do.
+names = []
+
 choice = ''
 display_title_bar()
 while choice != 'q':
+
+    coice = get_user_choice()
 
     # Response to user choice
     display_title_bar()
     if choice == '1':
         print("\nHere are the people I know.\n")
+        for name in names:
+            print(name.title())
     elif choice == '2':
-        print("\nI can't wait to meet this person!\n")
+        new_name = input("\nPlease tell me this person's name: ")
+        names.append(new_name)
+        print("\nI'm so happy to know %s!\n" % new_name.title())
     elif choice == 'q':
         print("\nThanks for playing. Bye.")
     else:
         print("\nI didn't understand that choice.\n")
-
-
-# Lets print a bunch of info in short intervals
-names = ['Aaron', 'Brenda', 'Cyrene', 'David', 'eric']
-
-# Prints each name 5 times
-for name in names:
-    display_title_bar()
-
-
-    print("\n\n")
-    for x in range(0, 5):
-        print(name.title())
-    
-    # Pause for 1 second between cycles
-    sleep(1)
